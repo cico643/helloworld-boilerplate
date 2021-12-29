422bd2fe38eed22a531beb703c5b376aceb1b6e6
Cihat Yeşildağ <cico643@hotmail.com.tr>
2021-12-29 15:05:12 +0300

    Added sample component to reproduce the issue

--- scripts/pages/page1.ts
+++ scripts/pages/page1.ts
@@ -4,46 +4,46 @@ import System from '@smartface/native/device/system';
 import Label from '@smartface/native/ui/label';
 
 export default class Page1 extends Page1Design {
-    constructor(private router: any) {
-        super();
-        this.btnNext.onPress = () => {
-            this.router.push('/pages/page2', { message: 'Hello World!' });
-        };
+  constructor(private router: any) {
+    super();
+    this.btnNext.onPress = () => {
+      this.router.push('/pages/page2', { message: 'Hello World!' });
+    };
+  }
+
+  /**
+   * @event onShow
+   * This event is called when a page appears on the screen (everytime).
+   */
+  onShow() {
+    super.onShow();
+    console.log('onShow Page1');
+    // this.headerBar.titleLayout.applyLayout();
+
+    this.lblInside.text = 'lblinside that will overflow into 2 lines';
+    // HERE WE CALCULATE THE HEIGHT OF THE LABEL MANUALLY AND
+    // DISPATCH THAT HEIGHT TO THE PARENT WRAPPER
+    this.flInfoWrapper.dispatch({
+      type: 'updateUserStyle',
+      userStyle: { height: 50 },
+    });
+    this.flInfoWrapper.applyLayout();
+
+    this.headerBar.titleLayout.applyLayout();
+  }
+
+  /**
+   * @event onLoad
+   * This event is called once when page is created.
+   */
+  onLoad() {
+    super.onLoad();
+    console.log('Onload Page1 : ', super.onLoad);
+    this.headerBar.leftItemEnabled = false;
+    this.headerBar.titleLayout = new PageTitleLayout();
+    this.addStyleableChild(this.headerBar.titleLayout, 'titleLayout');
+    if (System.OS === 'Android') {
+      this.headerBar.title = '';
     }
-
-
-    /**
-     * @event onShow
-     * This event is called when a page appears on the screen (everytime).
-     */
-    onShow() {
-        super.onShow();
-        console.log("onShow Page1");
-        // this.headerBar.titleLayout.applyLayout();
-        const lbl = new Label();
-        this.addChild(lbl, 'page1lbl1unique');
-        this.addStyleableChild(lbl, "page1lbl1unique", 'sf-label', (userProps: Record<string, any>) => {
-            return {...userProps, height: 50};
-        });
-
-        lbl.text = "It's a runtime label";
-
-        this.headerBar.titleLayout.applyLayout();
-    }
-
-    /**
-     * @event onLoad
-     * This event is called once when page is created.
-     */
-    onLoad() {
-        super.onLoad();
-        console.log("Onload Page1 : ", super.onLoad);
-        this.headerBar.leftItemEnabled = false;
-        this.headerBar.titleLayout = new PageTitleLayout();
-        this.addStyleableChild(this.headerBar.titleLayout, "titleLayout");
-        if (System.OS === 'Android') {
-            this.headerBar.title = '';
-        }
-    }
-
+  }
 }
